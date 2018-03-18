af8a0f9edd933b699fad03aa4bc8fc96981d30db
Zhenia Trochun <zheniatrochun@gmail.com>
2018-03-18 21:13:31 +0300

    lab3 base added

---
+++ Lab3/Sharp/Sharp/Data.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Threading;
+
+namespace Sharp
+{
+    class Data
+    {
+        public static int[][] CreateMatrix(int n)
+        {
+            int[][] m = new int[n][];
+
+            for (int i = 0; i < n; i++)
+            {
+                m[i] = new int[n];
+
+                for (int j = 0; j < n; j++)
+                    m[i][j] = 1;
+            }
+
+            return m;
+        }
+
+        public static void PrintMatrix(int[][] m)
+        {
+            for (int i = 0; i < m.Length; i++)
+            {
+                for (int j = 0; j < m[i].Length; j++)
+                    Console.Write(m[i][j] + "\t");
+                Console.WriteLine();
+            }
+        }
+
+        public static void MultiplyPart(int[][] a, int[][] b, int[][] result, int start, int end, CountdownEvent countdown)
+        {
+            for (int row = start; row < end; row++)
+                for (int col = 0; col < b.Length; col++)
+                {
+                    result[row][col] = 0;
+                    for (int k = 0; k < b.Length; k++)
+                        result[row][col] += a[row][k] * b[k][col];
+                }
+            countdown.Signal();
+        }
+
+        public static int[][] MultByNum(int[][] m, int h)
+        {
+            for (int i = 0; i < m.Length; i++)
+                for (int j = 0; j < m.Length; j++)
+                    m[i][j] *= h;
+
+            return m;
+        }
+    }
+}
---
+++ Lab3/Sharp/Sharp/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Threading;
+
+namespace Sharp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = 10;
+            int threadNum = 4;          
+
+            CountdownEvent countdown = new CountdownEvent(threadNum);
+
+            int[][] MF = Data.CreateMatrix(n);
+            int[][] MG = Data.CreateMatrix(n);
+            int h = 5;
+
+            int[][] result = new int[n][];
+            for (int i = 0; i < n; i++)
+                result[i] = new int[n];
+
+            StartMult(MF, MG, result, threadNum, countdown);
+
+            countdown.Wait();
+
+            MF = Data.MultByNum(result, h);
+
+            Data.PrintMatrix(MF);
+
+            Console.Read();
+        }
+
+        private static void RecursiveStart(int[][] a, int[][] b, int[][] result, CountdownEvent countdown, int i, int prev, int count, int threads, int additional)
+        {
+            int cnt = ((i == 0) ? count + additional : count);
+            new Thread(() => Data.MultiplyPart(a, b, result, prev, prev + cnt + threads - 1, countdown)).Start();
+
+            if (i != 0)
+                RecursiveStart(a, b, result, countdown, i - 1, prev + cnt - 1, count, threads, additional);
+        }
+
+        private static void StartMult(int[][] a, int[][] b, int[][] result, int threads, CountdownEvent countdown)
+        {
+            int n = a.Length;
+            int count = n / threads;
+            int additional = n % threads;
+
+            RecursiveStart(a, b, result, countdown, threads - 1, 0, count, threads, additional);
+        }
+    }
+}
